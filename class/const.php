6cd951acf9b898b68a92199a0f9e9ea64fc7dc41
JEANS <enrique_sistema_5000@hotmail.com>
2021-08-09 18:35:32 -0500

    configurando para localhost

--- class/const.php
+++ class/const.php
@@ -1,7 +1,12 @@
 <?php
 
-define("SERVIDOR", "localhost");
+/* define("SERVIDOR", "localhost");
 define("USUARIO", "senshi_senshi");
 define("PASSWORD", "Eam!LH15E+([");
-define("BASEDATOS", "senshi_senshi");
+define("BASEDATOS", "senshi_senshi"); */
 /*define("COSTOENVIO", $costoEnvio);*/
+
+define("SERVIDOR", "localhost");
+define("USUARIO", "root");
+define("PASSWORD", "");
+define("BASEDATOS", "senshi");
